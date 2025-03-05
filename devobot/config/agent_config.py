e2210de400485fb43c0cd21806f5f60efd33be13
George Gkenios <ggkenios@hotmail.com>
2025-03-05 18:17:59 +0100

    Small update with nodes

--- devobot/config/agent_config.py
+++ devobot/config/agent_config.py
@@ -1,24 +1,25 @@
 from enum import Enum
 
 from .yaml_config import Config, embeddings, llm
-from agent import classify, graph_node, rag
+import agent
 
 from services.database_vector import ChromaVectorDB
 
 
-wrapped_rag = graph_node(
-    rag,
+wrapped_rag = agent.graph_node(
+    getattr(agent, Config.config["agent"][2]["function"]),
     vector_db=ChromaVectorDB(collection_name="faq", embeddings=embeddings),
     llm=llm,
-    prompt=Config.config["agent"]["rag"]["prompt"],
+    prompt=Config.config["agent"][2]["function_params"]["prompt"],
 )
 
-wrapped_classify = graph_node(
-    classify,
+wrapped_classify = agent.graph_node(
+    getattr(agent, Config.config["agent"][1]["function"]),
     llm=llm,
-    prompt=Config.config["agent"]["intent"]["prompt"],
-    schema=Config.config["agent"]["intent"]["classes"],
+    prompt=Config.config["agent"][1]["function_params"]["prompt"],
+    schema=Config.config["agent"][1]["function_params"]["classes"],
 )
+
 
 class AgentNode(Enum):
     classify = wrapped_classify
