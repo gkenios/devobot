574a5cc9b286a432115e9d14cfb7808fd2db6d00
George Gkenios <ggkenios@hotmail.com>
2025-03-16 01:20:27 +0100

    It's alive

--- devobot/main.py
+++ devobot/main.py
@@ -9,15 +9,13 @@ from devobot.init.create_graph import graph
 QUESTION = "What time can I park my car in the Devoteam office?"
 
 
-async def main(graph: CompiledStateGraph):
-    async for msg, _ in graph.astream(
-        input=State(input=QUESTION, lineage=[]),
-        stream_mode="messages",
-    ):
+async def main(state: State, graph: CompiledStateGraph):
+    async for msg, _ in graph.astream(input=state, stream_mode="messages"):
         if msg.content:
             print(msg.content, end="", flush=True)
             await asyncio.sleep(0.05)
 
 
 if __name__ == "__main__":
-    asyncio.run(main(graph))
+    state = State(input=QUESTION, lineage=[])
+    asyncio.run(main(state, graph))
